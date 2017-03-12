2bcc499b6453b8d003f498db94e17aff21419cb0
the-simian <harlinjesse@gmail.com>
2017-03-11 22:08:07 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dorky-markov
+
+#how to run the project
+- ensure node is installed
+- `npm install`
+- `npm start`. The project is running on :3000
+- navigate to http://localhost:3000/index.html
---
+++ index.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var express = require('express');
+// static file compression middleware
+var compress = require('compression');
+// middleware to allow the general use of PUT and DELETE verbs
+var methodOverride = require('method-override');
+// logging middleware
+var morgan = require('morgan');
+// middleware to return X-Response-Time with a response
+var responseTime = require('response-time');
+
+var fs = require('fs');
+
+
+var app = express();
+app.use(morgan('dev'));
+app.use(responseTime());
+app.use(methodOverride());
+app.use(compress());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000);
+
+console.log('server started on port: ', process.env.PORT || 3000);
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 1337;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+  //  requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
